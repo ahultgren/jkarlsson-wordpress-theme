05865ca8a7fd81a5124b9b7955d799004ba76987
Andreas Hultgren <andreas.hultgren@frojd.se>
2012-10-24 16:07:56 +0200

    Cleaned up inconsistent tabs and line breaks

--- index.php
+++ index.php
@@ -1,31 +1,26 @@
 <?php get_header(); ?>
-	
-    <section id="content">
-		
-		<?php if (have_posts()) : ?>
-		
-			<?php while (have_posts()) : the_post(); ?>
-            
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h2 class="post-title"><?php the_title(); ?></h2>
-					<time datetime="<?php the_time('Y-m-d'); ?>" class="post-date"><?php the_time('F j, Y'); ?></time>
-					<?php the_content('Läs mer &raquo;'); ?>
-				</article>
-                
-			<?php endwhile; ?>
-			
-            <div class="navigation">
-				<div class="alignleft">
-					<?php next_posts_link('&laquo; Tidigare nyheter') ?>
-				</div>
-				<div class="alignright">
-					<?php previous_posts_link('Senare nyheter &raquo;') ?>
-				</div>
+	<section id="content">
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h2 class="post-title"><?php the_title(); ?></h2>
+				<time datetime="<?php the_time('Y-m-d'); ?>" class="post-date"><?php the_time('F j, Y'); ?></time>
+				<?php the_content('Läs mer &raquo;'); ?>
+			</article>
+
+		<?php endwhile; ?>
+
+		<div class="navigation">
+			<div class="alignleft">
+				<?php next_posts_link('&laquo; Tidigare nyheter') ?>
 			</div>
-			
-		<?php endif; ?>
-	
-    </section>
-		
+			<div class="alignright">
+				<?php previous_posts_link('Senare nyheter &raquo;') ?>
+			</div>
+		</div>
+
+	<?php endif; ?>
+	</section>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,21 +1,17 @@
 <?php get_header(); ?>
-	
-	<section id="content">
-		
-		<?php if (have_posts()) : ?>
-		
-			<?php while (have_posts()) : the_post(); ?>
-            
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="page-title"><?php the_title(); ?></h1>
-					<?php the_content(); ?>
-				</article>
-                
-			<?php endwhile; ?>
-			
-		<?php endif; ?>
-	
-	</section>
-		
+
+<section id="content">
+	<?php if (have_posts()) : ?>
+		<?php while (have_posts()) : the_post(); ?>
+
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<h1 class="page-title"><?php the_title(); ?></h1>
+				<?php the_content(); ?>
+			</article>
+
+		<?php endwhile; ?>
+	<?php endif; ?>
+</section>
+
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
